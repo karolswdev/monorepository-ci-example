12e2f0a3b62daf364f3c01565663a2325dd25c8e
Karol <karolsane@gmail.com>
2020-10-05 06:58:50 -0600

    Attempt #10

--- ci/calculate-buildInfo.ps1
+++ ci/calculate-buildInfo.ps1
@@ -7,9 +7,11 @@ param (
 $ErrorActionPreference = 'stop'
 
 # compute path to library from environment variable local build
+$toolPath = "./buildtools"
+
 # include build tools library ('source' the file)
 # we now have access to all the functions in library
-. ./ci/workflow-utils.ps1 -expectMajorVersion:1 -expectMinorVersion:2
+. $toolPath/pwsh/workflow-utils.ps1 -expectMajorVersion:1 -expectMinorVersion:2
 
 # ----------------------------- TOOLS ARE INITIALIZED ----------------------------
 
@@ -28,7 +30,6 @@ $isReleaseVersion = Get-IsReleaseVersion -isLocalBuild:$isLocalBuild -eventName:
 $Product_Version = $env:BCT_PRODUCT_VERSION_BASE
 if (-not $isReleaseVersion) {
     $Product_Version = "$Product_Version-$prefix$buildTime-$branchHash"
-	$PrereleaseSuffix = "-$prefix$buildTime-$branchHash"
 }
 
 $isPublishing = Get-IsPublishing -isReleaseVersion:$isReleaseVersion -branchName:$branchName
@@ -47,5 +48,5 @@ Set-BuildEnvironmentVariable BCT_BRANCH $branchName
 Set-BuildEnvironmentVariable BCT_IS_RELEASE_VERSION $isReleaseVersion
 Set-BuildEnvironmentVariable BCT_IS_PUBLISHING $isPublishing
 Set-BuildEnvironmentVariable BCT_GIT_SHA $branchHash
-# MONO REPO addition
+#MONO
 Set-BuildEnvironmentVariable BCT_PRERELEASE_SUFFIX = $PrereleaseSuffix
